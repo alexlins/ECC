92e2474547037ccc15cdeb09c0f0373bdd6eab61
Alexandre <lins.alexandre@gmail.com>
2018-09-07 22:39:41 -0300

    Busca de Encontros

---
+++ EccWin/Encontros/BuscaEncontro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EccWin.Encontros
+{
+    public partial class BuscaEncontro : Form
+    {
+        public BuscaEncontro()
+        {
+            InitializeComponent();
+        }
+    }
+}
