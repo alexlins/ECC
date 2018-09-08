633e259c8e41980fbfd2b157589d40571507a753
Mariana Leite <starmcl@gmail.com>
2018-09-07 22:47:48 -0300

    Criação do Busca Inscricao

---
+++ EccWin/Inscricao/BuscaInscricao.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace EccWin.Inscricao
+{
+    partial class BuscaInscricao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BuscaInscricao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 302);
+            this.Name = "BuscaInscricao";
+            this.Text = "BuscaInscricao";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
